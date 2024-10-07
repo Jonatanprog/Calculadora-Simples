15ea7324218e9e6c25868cfe5eaeb0f32605f151
Jonatan_Negreiros <j_oliveira@hotmail.com.br>
2024-10-07 11:51:05 -0300

    Calculadora simples em SH
    Mesmo modelo feito em Python.

---
+++ script_calculadora.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Esse é um script para calculadora
+
+#Solicitar operação
+
+num1 = int(input('Digite o primeiro número: '))
+
+operação=input ('''Por favor use
++ para adição
+- para subtração
+* para multiplicação
+/ para divisão   ''')
+
+num2 = int(input('Digite o segundo número: '))
+
+if operação == '+':
+	print('{} + {} = '.format(num1, num2))
+	print (num1 + num2)
+
+elif operação == "-":
+	print('{} - {} = '.format(num1, num2))
+	print (num1, num2)
+
+elif operação == '*':
+	print('{} * {} = '.format(num1, num2))
+	print (num1, num2)
+
+elif operação == '/':
+	print('{} / {} = '.format(num1, num2))
+	print (num1, num2)
+
+else:
+	print('Por favor use uma operação matemática')
+
